95d98a04b4acdf20be19d12e1078f497f6e652d8
JR Escert <uhoneprincemulaudzi@gmail.com>
2026-05-10 18:37:09 +0200

    feat: fixed url links

--- StudentPathGuideWebApp/src/java/com/apexcoders/web/CourseSuggestionServlet.java
+++ StudentPathGuideWebApp/src/java/com/apexcoders/web/CourseSuggestionServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 /**
@@ -29,6 +30,8 @@ public class CourseSuggestionServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
      
+        HttpSession session = request.getSession();
+        
         String term = request.getParameter("term");
       List<String> suggestions = cF.suggestionCourse(term);
       
--- StudentPathGuideWebApp/src/java/com/apexcoders/web/SignUpServlet.java
+++ StudentPathGuideWebApp/src/java/com/apexcoders/web/SignUpServlet.java
@@ -103,9 +103,9 @@ public class SignUpServlet extends HttpServlet {
         
         
        //get university course
-         List<UniversityCourses> filteredCourses = ucf.filterByFieldAndAps(field,aps);
-        request.setAttribute("course",filteredCourses);
-
+        List<UniversityCourses> filteredCourses = ucf.filterByFieldAndAps(field,aps);
+        session.setAttribute("course",filteredCourses);
+        session.setAttribute("stud",stud);
        
  
         // Forward to next page
